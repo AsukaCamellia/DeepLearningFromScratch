86f3a5627f497d20c3c284f162b5f831b8c87fac
Asuka <2232208323@qq.com>
2023-07-30 23:44:35 +0800

    绷不住了 跑不动！！！

--- 深度学习入门：基于Python的理论与实现/ch04/gradient_simplenet.py
+++ 深度学习入门：基于Python的理论与实现/ch04/gradient_simplenet.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
+#sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
+current_path = os.path.abspath(__file__)
+parent_path = os.path.abspath(os.path.dirname(current_path))
+print(os.getcwd())
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
